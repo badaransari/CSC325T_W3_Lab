5283dcbb1a06b9dfd8c23fd4963f1720001b2977
badaransari <badaransari@137.125.149.20>
2022-09-15 10:05:39 -0400

    Init class creation

--- CSC325_Fall22_lab01/src/main/java/edu/farmingdale/csc325_fall22_lab01/Driver.java
+++ CSC325_Fall22_lab01/src/main/java/edu/farmingdale/csc325_fall22_lab01/Driver.java
@@ -11,11 +11,6 @@ package edu.farmingdale.csc325_fall22_lab01;
  */
 public class Driver {
     
-    public static void main(String[] args) {
-        Student std = new Student();
-        
-        System.out.println("The student is" + std.toString());
-        
-    }
+  
     
 }
