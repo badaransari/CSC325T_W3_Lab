6b9067727bc9deae2703ea7387ab0114dc4dc559
badaransari <badaransari@137.125.149.20>
2022-09-15 10:20:55 -0400

    added setters and getters

--- CSC325T/src/main/java/edu/farmingdale/csc325t/Student.java
+++ CSC325T/src/main/java/edu/farmingdale/csc325t/Student.java
@@ -9,9 +9,36 @@ package edu.farmingdale.csc325t;
  *
  * @author badaransari
  */
+
 public class Student {
     String fName;
     String lName;
     double gpa;
+
+    public String getfName() {
+        return fName;
+    }
+
+    public String getlName() {
+        return lName;
+    }
+
+    public double getGpa() {
+        return gpa;
+    }
+
+    public void setfName(String fName) {
+        this.fName = fName;
+    }
+
+    public void setlName(String lName) {
+        this.lName = lName;
+    }
+
+    public void setGpa(double gpa) {
+        this.gpa = gpa;
+    }
+    
+    
    
 }
