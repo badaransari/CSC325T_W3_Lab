8ba857ae89bd00096607cc1c69063ecab1f27542
badaransari <badaransari@137.125.149.20>
2022-09-15 10:07:55 -0400

    code added

--- CSC325_Fall22_lab01/src/main/java/edu/farmingdale/csc325_fall22_lab01/Driver.java
+++ CSC325_Fall22_lab01/src/main/java/edu/farmingdale/csc325_fall22_lab01/Driver.java
@@ -13,6 +13,11 @@ public class Driver {
     
     public static void main(String[] args) {
     
+         
+        Student std = new Student();
+        
+        System.out.println("The student is" + std.toString());
+    
     
     }
  }
